a0ddbfa95d5726bf6dd8ea892a1fd85b09671f8b
Nicolas  Gaviria Mejia <nicolasgaviriamejia@nicolass-mbp.lan>
2018-09-11 16:28:10 -0500

    lint fixes

--- api/validators/accountValidator.js
+++ api/validators/accountValidator.js
@@ -1,11 +1,11 @@
 const yup = require('yup');
 
-const regexDomain = /([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}/
+const regexDomain = /([a-zA-Z0-9]([a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}/;
 
 const schema = yup.object().shape({
   domain: yup.string().matches(regexDomain).required(),
   identification: yup.string().required(),
-  accountId: yup.string().required()
+  accountId: yup.string().required(),
 });
 
 module.exports.validate = function validate(data) {
